33b7fd64fe5dd7890b43adfb01b08c2c497750b8
Michael Farrell <micolous+git@gmail.com>
2011-06-04 23:05:05 +0930

    Added licensing information to all files.

--- ApplicationLauncher/ConfigurationFileFormatException.cs
+++ ApplicationLauncher/ConfigurationFileFormatException.cs
@@ -1,6 +1,21 @@
+#region LICENSE
+//ApplicationLauncher: A limited shell-replacement launcher program
+//for Windows.
+
+//Copyright 2007, 2008, 2011 Michael Farrell <http://micolous.id.au/>
+
+//This program is free software: you can redistribute it and/or modify
+//it under the terms of the GNU General Public License as published by
+//the Free Software Foundation, either version 3 of the License, or
+//(at your option) any later version.
+
+//This program is distributed in the hope that it will be useful,
+//but WITHOUT ANY WARRANTY; without even the implied warranty of
+//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//GNU General Public License for more details.
+#endregion
+
 using System;
-using System.Collections.Generic;
-using System.Text;
 
 namespace ApplicationLauncher
 {
